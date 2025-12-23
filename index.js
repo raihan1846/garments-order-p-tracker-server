f7737f91280b5b761b55e869854e73a5d0a1a321
raihan1846 <raihan.bd2022@gmail.com>
2025-12-23 23:47:05 +0600

    add product api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -8,10 +8,41 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+const uri = "mongodb+srv://garmentsorderproductiontracker:w64Wp9VVTZ0nF6tc@cleancommunity.elxtjut.mongodb.net/?appName=CleanCommunity";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 
 app.get('/', (req, res) => {
     res.send('server is Running..!!!')
 })
+
+async function run(){
+try{
+    await client.connect();
+
+    const db = client.db('garments_order_productions');
+    const productCollection = db.collection('products');
+
+    app.post('/products', async(req,res)=>{
+        const newProduct = req.body;
+        const result = await productCollection.insertOne(newProduct);
+        res.send(result);
+    })
+
+    await client.db("admin").command({ping : 1});
+    console.log("Ping Database");
+}
+finally{
+
+}
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`Server is running, ${port}`);
